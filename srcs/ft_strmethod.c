4cc809ba3022df65d2e32f732199363582b3bbed
jwebb <jwebb@student.42.us.org>
2017-06-21 16:21:16 -0700

    testing changes to ft_strmethod.c

--- srcs/ft_strmethod.c
+++ srcs/ft_strmethod.c
@@ -168,6 +168,87 @@ char	*new_str(void)
 	return (str);
 }
 
+void	num_precision(char **str, t_flag *flags, unsigned int len)
+{
+	if (*str[0] == '-' && len < flags->prec)
+		add_chars(str, '0', flags->prec - len + 1, flags);
+	else if (len < flags->prec)
+		add_chars(str, '0', flags->prec - len, NULL);
+	else if (!ft_strcmp(*str, "0") && !flags->hash)
+	{
+		*str = ft_memalloc(1);
+		*str[0] = '\0';
+	}
+}
+
+void	add_precision(char **str, t_flag *flags, unsigned int len, char c)
+{
+	if (flags->buff > flags->prec && !(flags->buff == flags->prec + 1 &&
+		NUM_FLAGS && *str[0] == '-'))
+		c = ' ';
+	if ((flags->s || flags->ls) && len > flags->prec)
+		*str = (char*)ft_realloc(*str, flags->prec);
+	if ((NUM_FLAGS || flags->x || flags->xx || flags->o || flags->lo))
+		num_precision(str, flags, len);
+	if (!flags->prec && ((flags->s || flags->ls || flags->x || flags->xx) ||
+			(flags->o && !flags->hash && !ft_strcmp(*str, "0"))))
+		*str = new_str();
+	len = ft_strlen(*str);
+	if (flags->buff && flags->sign && ft_isdigit(*str[0]) && c == ' ' &&
+			!flags->u && !flags->lu)
+		add_chars(str, '+', 1, NULL);
+	if (flags->buff > len)
+		if (!((flags->u || flags->lu) && c == ' '))
+			add_chars(str, c,
+				flags->buff - flags->sign - flags->space - len, flags);
+}
+
+void	init_mods(char **str, t_flag *flags, char *c, unsigned int *len)
+{
+	*len = ft_strlen(*str);
+	*c = ' ';
+	if ((flags->zero && !flags->left) || (flags->o && flags->dot &&
+			flags->buff < flags->prec))
+		*c = '0';
+	if ((flags->x || flags->xx) && flags->hash && ft_strcmp(*str, "0"))
+		*len += 2;
+	if ((flags->x || flags->xx) && flags->hash && ft_strcmp(*str, "0")
+			&& !flags->zero)
+		add_xprefix(str);
+	if (flags->o && flags->hash && ft_strcmp(*str, "0"))
+		*len += add_chars(str, '0', 1, NULL);
+	if (flags->zero && flags->sign && *str[0] != '-')
+		++*len;
+}
+
+void	apply_mods(char **str, t_flag *flags, const void *arg)
+{
+	unsigned int	len;
+	int				i;
+	char			c;
+
+	init_mods(str, flags, &c, &len);
+	if (flags->dot)
+		add_precision(str, flags, len, c);
+	else if (flags->buff > len)
+	{
+		if (flags->c && !arg)
+			i = 1;
+		else
+			i = flags->buff;
+		add_chars(str, c, i - len, flags);
+	}
+	if ((flags->x || flags->xx) && flags->hash && ft_strcmp(*str, "0")
+			&& flags->zero && *str[0])
+		add_xprefix(str);
+	if (flags->sign && NUM_FLAGS && !flags->u && !flags->lu &&
+			ft_isdigit(*str[0]))
+		add_chars(str, '+', 1, NULL);
+	else if (flags->space && NUM_FLAGS && !flags->u && !flags->lu &&
+			*str[0] != '-' && (!flags->buff || flags->dot))
+		add_chars(str, ' ', 1, NULL);
+}
+/*
 void	apply_mods(char **str, t_flag *flags, const void *arg)
 {
 	char	*tmp;
@@ -243,7 +324,7 @@ void	apply_mods(char **str, t_flag *flags, const void *arg)
 			*str[0] != '-' && (!flags->buff || flags->dot))
 		add_chars(str, ' ', 1, NULL);
 }
-
+*/
 int		print_r(const void *arg)
 {
 	char			*style;
